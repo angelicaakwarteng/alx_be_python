aa0f83df82a8d848a71abc4cb5d66279c8c02bf2
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-07 15:41:44 +0000

    Added a Personal Finance Calculator file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
 # calculating monthly savings based on inputted monthly income and expenses at a fixed rate of 5%.
-month_income = float(input("Enter your monthly income: "))
+month_income = input("Enter your monthly income: ")
 total_month_expenses = float(input("Enter your total monthly expenses: "))
 month_savings = month_income - total_month_expenses
 projected_savings = month_savings * 12 + (month_savings * 12 * 0.05)
