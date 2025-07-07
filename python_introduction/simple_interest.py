d11b9af6e33e3002947b7c94d02c5c4aec0ddcb9
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-07 14:16:08 +0000

    Updated interest formula

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# calculating the simple interest of 1000 at an annual rate of 5% over 3 years
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal*rate*time
+
+print(f"The simple interest is: {interest}")
+print(interest)
