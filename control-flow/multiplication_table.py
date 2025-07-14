161389473756fa601c054e6211a9dcf8d5b6789c
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-14 13:24:06 +0000

    Multiplication table file

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+#Using a for loop to generate and print the multiplication table for a given number.
+number = int(input("Enter a number to see its multiplication table:"))
+for i in range(1, 11):
+    table = str(number) + " * " + str(i)
+    answer = number*i
+    print(f"{table} = {answer}")
