61928ff4693061ddc48bad97eb2a0659e53c065b
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-26 20:59:53 +0000

    Add new file shopping_list_manager.py

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,13 +1,42 @@
-from arithmetic_operations import perform_operation
+# from arithmetic_operations import perform_operation
+
+# def main():
+#     print("Arithmetic Operations")
+#     num1 = float(input("Enter the first number: "))
+#     num2 = float(input("Enter the second number: "))
+#     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+#     result = perform_operation(num1, num2, operation)
+#     print(f"Result: {result}")
+
+# if __name__ == "__main__":
+#     main()
+
+from shopping_list_manager import display_menu
 
 def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+        if choice == '1':
+            # Prompt for and add an item
+            item = str(input('Add an item: '))
+            shopping_list.append(item)
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = str(input('Add an item'))
+            shopping_list.remove(item)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,7 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
