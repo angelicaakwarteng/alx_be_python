fd51f439dd8a9b1b04187cefde12824ee1b80777
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-26 20:13:50 +0000

    updated operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,7 +4,7 @@ def perform_operation(num1, num2, operation):
             return num1 + num2
         case 'subtract':
             return num1 - num2
-        case 'multiplication':
+        case 'multiply':
             return num1 * num2
-        case division:
+        case 'divide':
             return num1 / num2
