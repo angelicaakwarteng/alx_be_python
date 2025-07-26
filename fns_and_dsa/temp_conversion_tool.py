42e0e00e09c5574aea7bd42c626e61726044e0df
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-26 21:57:40 +0000

    Add new file temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -16,8 +16,8 @@ def convert_to_fahrenheit(celsius):
 
 # User interaction
 def main():
-        temp_input = input("Enter the temperature to convert (e.g., 32): ")
-        unit = input("Is this in Celsius or Fahrenheit? (C/F): ").strip().upper()
+        temp_input = input("Enter the temperature to convert: ")
+        unit = input("Is this in Celsius or Fahrenheit? (C/F): ")
 
         # Validate the temperature value
         temperature = float(temp_input)
