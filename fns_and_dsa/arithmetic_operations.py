8f06e76f9f2ed37c63b2fad7d63ce4087440cb29
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-26 20:11:12 +0000

    created new folder fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,10 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiplication':
+            return num1 * num2
+        case division:
+            return num1 / num2
