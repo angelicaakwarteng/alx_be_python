a058dce32fdb17357ff21c91374a0c4ebed38b2c
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-07-30 13:20:41 +0000

    Created new directory Programming Paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,9 +1,11 @@
-class BankAccount():
+class BankAccount:
     def __init__(self, initial_balance):
         self.account_balance = initial_balance
 
     def deposit(self, amount):
+        BankAccount = 0
         self.account_balance += amount
+        return BankAccount.append(self.account_balance)
 
     def withdraw(self, amount):
         self.account_balance -= amount
